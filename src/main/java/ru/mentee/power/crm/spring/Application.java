06a1df19f6ab982e32f6251621764d08e180eaae
Alexandr Shpet <necromant45z@gmail.com>
2026-05-27 22:36:03 +0300

    BCORE-15: Connecting Spring MVC to JTE

--- src/main/java/ru/mentee/power/crm/spring/Application.java
+++ src/main/java/ru/mentee/power/crm/spring/Application.java
@@ -2,10 +2,18 @@ package ru.mentee.power.crm.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.mentee.power.crm.service.LeadService;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.mentee.power.crm")
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        LeadService leadService = context.getBean(LeadService.class);
+
+        for(int i = 0; i < 5; i++) {
+            leadService.addLead("lead" + i + "@test.com", "+12345" + i, "Company" + i, "NEW");
+        }
+        leadService.addLead("<script>alert('XSS')</script>", "+000000", "SomeCompany", "NEW");
     }
 }
---
+++ src/test/java/ru/mentee/power/crm/spring/controller/LeadControllerTest.java
@@ -0,0 +1,27 @@
+package ru.mentee.power.crm.spring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LeadControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnStatus200OK_whenGetLeads() throws Exception {
+        mockMvc.perform(get("/leads"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Email")));
+    }
+}
